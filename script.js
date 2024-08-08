4a4e82c13f2e67f0a9535a68791b9583c3daabc2
Uzairak01 <h.uzairak01@gmail.com>
2024-08-08 18:22:22 +0500

    updates

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+window.addEventListener('scroll', function () {
+    const upbtnScroll = document.querySelector('.upbtn');
+    if(window.scrollY > 100){
+        upbtnScroll.classList.add('upbtnScrolled');
+    }else{
+        upbtnScroll.classList.remove('upbtnScrolled');
+    }
+});
